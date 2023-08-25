29503cbfb3673738a17036fcf0628d84ae3c561a
Pavel Borshchevsky <BorshchevskyPA@pba.su>
2023-08-25 11:21:29 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,3 @@
+## Base fullstack template
+
+A template that combines backend (Django REST) and frontend (Vue.js) into one fullstack-project. Can be used to create new fullstack projects.
--- backend/run.sh
+++ backend/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 ./wait-for-it.sh postgres:5432 -t 15
 python manage.py makemigrations
 python manage.py migrate
 python manage.py add_test_query
+python manage.py collectstatic
 gunicorn --bind 0.0.0.0:8000 main.wsgi --reload
